7a3e303f4dbf80049eb2565d060a02b00a4abc43
ohyung1287 <ohyung1287@gmail.com>
2021-09-15 13:32:10 -0400

    fix

--- contracts/abstracts/Convertible.sol
+++ contracts/abstracts/Convertible.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.8.7;
 
 import "../openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 import "../openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "../openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../openzeppelin/contracts-upgradeable/token/ERC20/IERC20.sol";
 import "../libs/GluwacoinModels.sol";
 import "../libs/HashMapIndex.sol";
 
